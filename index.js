8e84af4f8b6f21375ea276b49af59253f0ac578f
Sandra Salazar <sandyarlet@hotmail.com>
2023-04-11 21:56:03 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById('btn');
+
+const hi = document.getElementById('hi');
+
+btn.addEventListener('click', function handleClick() {
+    hi.innerHTML= "Hola GitHub";
+})
